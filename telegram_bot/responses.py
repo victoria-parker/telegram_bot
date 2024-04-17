96e62d68c56cf0f664ba7affe2f63b00d4c73c01
victoria-parker <victoriaparkerb@gmail.com>
2024-04-17 14:01:36 +0200

    extra hellos options added for responses when greeting

--- telegram_bot/responses.py
+++ telegram_bot/responses.py
@@ -4,7 +4,7 @@ from telegram import Update
 def generate_text(text: str, update:Update) -> str:
     processed: str = update.message.text.lower()
 
-    if 'hello' in processed:
+    if 'hello' or 'hi' or 'hey' in processed:
         return f'Hello {update.effective_user.first_name}'
 
     return ' I generally do not talk much, I mainly process audio messages and detect faces in pictures. Send me an audio message or a picture and I will be happy'
