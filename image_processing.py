ec9b73bb81b820589931904be78c12f58c554c60
victoria-parker <victoriaparkerb@gmail.com>
2024-04-17 15:32:27 +0200

    logging error added to image processing

--- image_processing.py
+++ image_processing.py
@@ -3,40 +3,50 @@ import cv2
 import os
 from datetime import datetime
 import shutil
+import logging
 
 async def process_image(file_path: str):
-    detection_face = await image_face_detector(file_path)
-    if detection_face:
-        destination_path = await create_file_name(file_path)
-        shutil.copyfile(file_path, destination_path)
-        return destination_path
-    else:
-        return False
+    try:
+        detection_face = await image_face_detector(file_path)
+        if detection_face:
+            destination_path = await create_file_name(file_path)
+            shutil.copyfile(file_path, destination_path)
+            return destination_path
+        else:
+            return False
+    except Exception as e:
+        logging.error(f'Error processing image: {str(e)}')
 
 async def image_face_detector(file_path: str):
-    image = cv2.imread(file_path)
-    face_locations = face_recognition.face_locations(image)
-    if face_locations:
-        return True
-    else:
-        return False
+    try:
+        image = cv2.imread(file_path)
+        face_locations = face_recognition.face_locations(image)
+        if face_locations:
+            return True
+        else:
+            return False
+    except Exception as e:
+        logging.error(f'Error at image face detector: {str(e)}')
     
 async def create_file_name(file_path: str):
-    #get user id
-    user_id = os.path.basename(file_path).split('_')[0]
-    
-    #get timestamp to use in the new name
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    try:
+        #get user id
+        user_id = os.path.basename(file_path).split('_')[0]
+        
+        #get timestamp to use in the new name
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    #get file extention
-    file_extension = os.path.splitext(file_path)[1]
+        #get file extention
+        file_extension = os.path.splitext(file_path)[1]
 
-    #create new file name
-    new_file_name = f"image_message_{user_id}_{timestamp}{file_extension}"
+        #create new file name
+        new_file_name = f"image_message_{user_id}_{timestamp}{file_extension}"
 
-    #create the file path
-    user_audio_dir = os.path.join("media", "images", str(user_id))
-    os.makedirs(user_audio_dir, exist_ok=True)
-    new_file_path = os.path.join(user_audio_dir, new_file_name)
+        #create the file path
+        user_audio_dir = os.path.join("media", "images", str(user_id))
+        os.makedirs(user_audio_dir, exist_ok=True)
+        new_file_path = os.path.join(user_audio_dir, new_file_name)
 
-    return new_file_path
+        return new_file_path
+    except Exception as e:
+        logging.error(f'Error at create file name for image: {str(e)}')
