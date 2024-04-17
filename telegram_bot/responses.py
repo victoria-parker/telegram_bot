d044861b5d6f8c5253f74948c6c1f6d6f87804b7
victoria-parker <victoriaparkerb@gmail.com>
2024-04-17 15:21:21 +0200

    correction in generate text for the greeting

--- telegram_bot/responses.py
+++ telegram_bot/responses.py
@@ -4,7 +4,7 @@ from telegram import Update
 def generate_text(text: str, update:Update) -> str:
     processed: str = update.message.text.lower()
 
-    if 'hello' or 'hi' or 'hey' in processed:
+    if 'hello' in processed or 'hi' in processed or 'hey' in processed:
         return f'Hello {update.effective_user.first_name}'
 
     return 'Hey there! While I may not chat much, I excel at processing audio messages and detecting faces in pictures. Feel free to send me an audio message or a picture, and I\'ll be more than happy to assist you!'
