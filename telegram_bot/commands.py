3cf0c4c34999ee186693c54c900909e69e3a3326
victoria-parker <victoriaparkerb@gmail.com>
2024-04-17 14:14:00 +0200

    help command text format improved

--- telegram_bot/commands.py
+++ telegram_bot/commands.py
@@ -5,16 +5,12 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Welcome to AudioFaceBot! I'm here to assist you in saving audio messages and detecting faces in photos. When sending audio messages, they will be saved in WAV format with a sampling rate of 16kHz. For photos, I'll only save those that contain faces. Feel free to send me an audio messages or photos, and I'll take care of the rest.")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        '''Sure! Here's how you can use AudioFaceBot:
-
-            Start: Begin interacting with the bot.
-            
-            Send Audio Messages: Share audio messages from your dialogues, and they'll be saved in WAV format with a sampling rate of 16kHz.
-
-            Send Photos: Share photos, and I'll determine if there are faces present before saving them.
-            
-            Help: You're here! Get assistance and learn how to use the bot effectively.
-
-            ''')    
+    help_text = (
+        "Sure! Here's how you can use AudioFaceBot:\n\n"
+        "• Start: Begin interacting with the bot.\n"
+        "• Send Audio Messages: Share audio messages from your dialogues, and they'll be saved in WAV format with a sampling rate of 16kHz.\n"
+        "• Send Photos: Share photos, and I'll determine if there are faces present before saving them.\n"
+        "• Help: You're here! Get assistance and learn how to use the bot effectively."
+    )
+    await update.message.reply_text(help_text)    
 
